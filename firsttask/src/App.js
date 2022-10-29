76759ddc92c7d2c7f157d2ec5121a4fa2602deb0
Favourshaw <favourvanderwaal@gmail.com>
2022-10-29 17:11:51 +0200

    added profile picture and links+titles

--- firsttask/src/App.js
+++ firsttask/src/App.js
@@ -28,24 +28,25 @@ function App() {
 
       </div>
 
-      <img src="images/logo.svg" alt="Avatar" className="avatar" id="profile__img"/><br />
+      <img src="images/pp.jpg" alt="Avatar" className="profile" id="profile__img"/><br />
 
 
-      <h1 className="hname">Annette Black</h1><br />
+      <h1 className="hname" id="twitter">favour_shaww</h1><br />
+      <h1 className="hname2" id="slack">shawghost</h1>
       <div className="tabs">
 
         
-        <div className='mainy'><a className="btn" id="twitter"><p className="text">Twitter Link</p></a></div><br />
-        <div className='mainy'><a className="btn" id="btn__zuri"><p className="text">Zuri Team</p></a></div><br />
-        <div className='mainy'><a className="btn" id="books"><p className="text">Zuri Books</p></a></div><br />
-        <div className='mainy'><a className="btn" id="book__python"><p className="text">Python Books</p></a></div><br />
-        <div className='mainy'><a className="btn" id="pitch"><p className="text">Background Check for Coders</p></a></div><br />
-        <div className='mainy'><a className="btn" id="book__design"><p className="text">Design Books</p></a></div><br />
+        <div className='mainy'><a href="https://twitter.com/favour_shaww/" className="btn" id="twitter"><p className="text">Twitter Link</p></a></div><br />
+        <div className='mainy'><a href="https://training.zuri.team/" className="btn" id="btn__zuri"><p className="text">Zuri Team</p></a></div><br />
+        <div className='mainy'><a title="you can find any book for design and coding when you click on this link"  href="https://books.zuri.team/python-for-beginners?ref_id=shawghost" className="btn" id="books"><p className="text">Zuri Books</p></a></div><br />
+        <div className='mainy'><a title="Purchase my newest book on python here" href="https://books.zuri.team/python-for-beginners?ref_id=shawghost" className="btn" id="book__python"><p className="text">Python Books</p></a></div><br />
+        <div className='mainy'><a title="Planing on hiring a new developer?, click here to perform a full background check before hiring" href="https://background.zuri.team" className="btn" id="pitch"><p className="text">Background Check for Coders</p></a></div><br />
+        <div className='mainy'><a title="Zuri is offering a free design book, click here to grab one." href="https://books.zuri.team/design-rules" className="btn" id="book__design"><p className="text">Design Books</p></a></div><br />
       </div>
 
       
         <div className="btimg" >
-          <img src="images/slack.svg" alt="Avatar" /><img src="images/git.svg" alt="Avatar" />
+          <a  className="btimg" ><img src="images/slack.svg" alt="Avatar" /></a><a href="https://github.com/Favourshaw" className="btimg" ><img src="images/git.svg" alt="Avatar" /></a>
         </div>
 
      
